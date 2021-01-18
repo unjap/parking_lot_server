6f18451af1803500ab542940b3f7e680764847f7
nk1224 <nk1224@nate.com>
2021-01-18 17:49:20 +0900

    [nk1224] 주차장 검색 쿼리 문 변경

--- database/sqlMapper.js
+++ database/sqlMapper.js
@@ -2,7 +2,7 @@ let result = null;
 module.exports = class sqlMapper {
   // 주차장 리스트 가져오기
   static getList(searchRegion, callback) {
-    global.connection.query('SELECT * FROM list WHERE lnmadr LIKE ?', [searchRegion + "%"], function (err, rows) {
+    global.connection.query('SELECT * FROM list WHERE lnmadr REGEXP ?', searchRegion, function (err, rows) {
       if (!err) {
         result = rows;
       } else {
